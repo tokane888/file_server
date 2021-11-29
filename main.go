de22d30dcf815bf0a3e5ea6e9744fe1ee4cbc353
tokane888 <tokane888@gmail.com>
2021-11-30 06:50:03 +0900

    listen開始時のエラー出力追加

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
 	fileServer := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static", fileServer))
-	http.ListenAndServe(":8081", nil)
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
